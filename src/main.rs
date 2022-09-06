631fd56ce54c4805ff5f0f2be4c0baa9f8fd8ad1
yuiiio <atbjyk@protonmail.com>
2022-09-06 22:32:00 +0900

    timer base

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use serialport;
 use std::time::Duration;
 use std::thread::sleep;
 use std::f64::consts::PI;
+use std::time::SystemTime;
 
 fn generate_cmd<'a>(motor: &[i8; 3], cmd_str: &'a mut String) -> &'a [u8] {
 
@@ -48,36 +49,44 @@ fn main() {
                                         [MOTOR2_DIR.cos(), MOTOR2_DIR.sin()],
                                         [MOTOR3_DIR.cos(), MOTOR3_DIR.sin()]];
 
-    let direction_sceta :u16 = 0;// 0 ~ 360
-    let direction_sceta_dig : f64 = (direction_sceta as f64) / (360 as f64) * 2.0 * PI;
-    let direction_sceta_dig_x_y : [f64; 2] = [direction_sceta_dig.cos(), direction_sceta_dig.sin()];
+    let now = SystemTime::now();
 
-    /*
-    let motor1 :f64 = (direction_sceta_dig - MOTOR1_DIR).cos();
-    let motor2 :f64 = (direction_sceta_dig - MOTOR2_DIR).cos();
-    let motor3 :f64 = (direction_sceta_dig - MOTOR3_DIR).cos();
-    */
+    loop {
+        if now.elapsed().unwrap().as_secs_f64() >= 2.0 {
+            break;
+        }
 
-    let motor1 :f64 = direction_sceta_dig_x_y[0] * motor_dir_x_y[0][0] + direction_sceta_dig_x_y[1] * motor_dir_x_y[0][1];
-    let motor2 :f64 = direction_sceta_dig_x_y[0] * motor_dir_x_y[1][0] + direction_sceta_dig_x_y[1] * motor_dir_x_y[1][1];
-    let motor3 :f64 = direction_sceta_dig_x_y[0] * motor_dir_x_y[2][0] + direction_sceta_dig_x_y[1] * motor_dir_x_y[2][1];
+        let direction_sceta :u16 = 90;// 0 ~ 360
+        let direction_sceta_dig : f64 = (direction_sceta as f64) / (360 as f64) * 2.0 * PI;
+        let direction_sceta_dig_x_y : [f64; 2] = [direction_sceta_dig.cos(), direction_sceta_dig.sin()];
 
-    let motor :[i8; 3] = [(motor1*-50.0) as i8, (motor2*-50.0) as i8, (motor3*-50.0) as i8]; //should -100 to 100
-    let mut cmd_str =  String::from("1F000"); //unused motor channel 1
-    let cmd = generate_cmd(&motor, &mut cmd_str);
+        /*
+           let motor1 :f64 = (direction_sceta_dig - MOTOR1_DIR).cos();
+           let motor2 :f64 = (direction_sceta_dig - MOTOR2_DIR).cos();
+           let motor3 :f64 = (direction_sceta_dig - MOTOR3_DIR).cos();
+           */
 
-    println!("start motor(byte): {:?}", cmd);
+        let motor1 :f64 = direction_sceta_dig_x_y[0] * motor_dir_x_y[0][0] + direction_sceta_dig_x_y[1] * motor_dir_x_y[0][1];
+        let motor2 :f64 = direction_sceta_dig_x_y[0] * motor_dir_x_y[1][0] + direction_sceta_dig_x_y[1] * motor_dir_x_y[1][1];
+        let motor3 :f64 = direction_sceta_dig_x_y[0] * motor_dir_x_y[2][0] + direction_sceta_dig_x_y[1] * motor_dir_x_y[2][1];
 
-    /*
-    match port.write(cmd)
-    {
-        Ok(_) => println!("start motor: {:?}", cmd_str),
-        Err(e) => eprintln!("{:?}", e),
+        let motor :[i8; 3] = [(motor1*-50.0) as i8, (motor2*-50.0) as i8, (motor3*-50.0) as i8]; //should -100 to 100
+        let mut cmd_str =  String::from("1F000"); //unused motor channel 1
+        let cmd = generate_cmd(&motor, &mut cmd_str);
+
+        println!("start motor(byte): {:?}", cmd);
+
+        /*
+           match port.write(cmd)
+           {
+           Ok(_) => println!("start motor: {:?}", cmd_str),
+           Err(e) => eprintln!("{:?}", e),
+           }
+
+           port.flush().unwrap();
+           */
     }
-
-    port.flush().unwrap();
-
-    sleep(Duration::from_secs(10));
+    /*
 
     let poweroff_all_motor: &[u8; 21] = b"1F0002F0003F0004F000\n";
     //let force_stop_motor: &[u8; 21] = b"1R0002R0003R0004R000\n";
