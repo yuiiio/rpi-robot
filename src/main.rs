b444579b9788eee63f27b93305be80e6b0758f94
yuiiio <atbjyk@protonmail.com>
2022-10-01 06:51:56 +0900

    space space

--- src/main.rs
+++ src/main.rs
@@ -44,26 +44,26 @@ fn main() {
             ::std::process::exit(1);
     });
 
-    const MOTOR1_DIR :f64 = ((-60 + 90) as f64) / (360 as f64) * 2.0 * PI;
-    const MOTOR2_DIR :f64 = ((60 + 90) as f64) / (360 as f64) * 2.0 * PI;
-    const MOTOR3_DIR :f64 = ((180 + 90) as f64) / (360 as f64) * 2.0 * PI;
+    const MOTOR1_DIR: f64 = ((-60 + 90) as f64) / (360 as f64) * 2.0 * PI;
+    const MOTOR2_DIR: f64 = ((60 + 90) as f64) / (360 as f64) * 2.0 * PI;
+    const MOTOR3_DIR: f64 = ((180 + 90) as f64) / (360 as f64) * 2.0 * PI;
 
-    let motor_dir_x_y :[[f64; 2]; 3] = [[MOTOR1_DIR.cos(), MOTOR1_DIR.sin()],
+    let motor_dir_x_y: [[f64; 2]; 3] = [[MOTOR1_DIR.cos(), MOTOR1_DIR.sin()],
                                         [MOTOR2_DIR.cos(), MOTOR2_DIR.sin()],
                                         [MOTOR3_DIR.cos(), MOTOR3_DIR.sin()]];
                                         
-    let mut preval :[f64; 3] = [0.0, 0.0, 0.0];
+    let mut preval: [f64; 3] = [0.0, 0.0, 0.0];
     //used by PID
     let mut last_command_time: f64 = 0.0;
 
-    let mut error :[f64; 3] = [0.0, 0.0, 0.0];
-    let mut integral :f64 = 0.0;
+    let mut error: [f64; 3] = [0.0, 0.0, 0.0];
+    let mut integral: f64 = 0.0;
 
-    const KP :f64 = 0.6;
-    const KI :f64 = 10.0;
-    const KD :f64 = 0.00003;
+    const KP: f64 = 0.6;
+    const KI: f64 = 10.0;
+    const KD: f64 = 0.00003;
 
-    let from_controller_params :Arc<Mutex<(u16, u8)>> = Arc::new(Mutex::new((0, 0)));
+    let from_controller_params: Arc<Mutex<(u16, u8)>> = Arc::new(Mutex::new((0, 0)));
     let from_controller_params_clone = Arc::clone(&from_controller_params);
 
     let _handle = thread::spawn(move || {
@@ -71,22 +71,22 @@ fn main() {
             let mut from_controller_str = String::new();
             io::stdin().read_line(&mut from_controller_str).unwrap();
             let mut from_controller = from_controller_str.split(' ');
-            let direction_sceta :u16 = from_controller.next().unwrap().parse::<u16>().unwrap();
+            let direction_sceta: u16 = from_controller.next().unwrap().parse::<u16>().unwrap();
             let power_u8: u8 = from_controller.next().unwrap().trim_matches('\n').parse::<u8>().unwrap();
             let mut params = from_controller_params_clone.lock().unwrap();
             *params = (direction_sceta, power_u8);
         }
     });
 
-    let from_sensor_dir :Arc<Mutex<u16>> = Arc::new(Mutex::new(0));
+    let from_sensor_dir: Arc<Mutex<u16>> = Arc::new(Mutex::new(0));
     let from_sensor_dir_clone = Arc::clone(&from_sensor_dir);
 
     let _handle2 = thread::spawn(move || {
         //let ten_micros = time::Duration::from_micros(10);
         let mut spi0_0 = Spi::new( Bus::Spi0, SlaveSelect::Ss0, 1_000_000, spi::Mode::Mode0 ).expect( "Failed Spi::new" ); //1MHz
-        let write_data :Vec<u8> = vec![0x20];
-        let mut read_data1 :Vec<u8> = vec![0];
-        let mut read_data2 :Vec<u8> = vec![0];
+        let write_data: Vec<u8> = vec![0x20];
+        let mut read_data1: Vec<u8> = vec![0];
+        let mut read_data2: Vec<u8> = vec![0];
         loop{
             let _ret = spi0_0.write( &write_data );
             //thread::sleep(ten_micros);
@@ -102,7 +102,7 @@ fn main() {
         }
     });
 
-    let program_switch :Arc<Mutex<bool>> = Arc::new(Mutex::new(false));
+    let program_switch: Arc<Mutex<bool>> = Arc::new(Mutex::new(false));
     let program_switch_clone = Arc::clone(&program_switch);
     let _handle3 = thread::spawn(move || {
         let gpio = Gpio::new().unwrap();
@@ -124,7 +124,7 @@ fn main() {
     ];
     */
 
-    let BALL_pos_relative :Arc<Mutex<[i16; 2]>> = Arc::new(Mutex::new([0; 2]));
+    let BALL_pos_relative: Arc<Mutex<[i16; 2]>> = Arc::new(Mutex::new([0; 2]));
     let BALL_pos_relative_clone = Arc::clone(&BALL_pos_relative);
 
     let _handle4 = thread::spawn(move || {
@@ -135,14 +135,14 @@ fn main() {
         let write_data :Vec<u8> = vec![0x40];
         let mut read_first_seg :Vec<u8> = vec![0];
 
-        let mut read_data1_H :Vec<u8> = vec![0];
-        let mut read_data1_L :Vec<u8> = vec![0];
-        let mut read_data2_H :Vec<u8> = vec![0];
-        let mut read_data2_L :Vec<u8> = vec![0];
-        let mut read_data3_H :Vec<u8> = vec![0];
-        let mut read_data3_L :Vec<u8> = vec![0];
-        let mut read_data4_H :Vec<u8> = vec![0];
-        let mut read_data4_L :Vec<u8> = vec![0];
+        let mut read_data1_H: Vec<u8> = vec![0];
+        let mut read_data1_L: Vec<u8> = vec![0];
+        let mut read_data2_H: Vec<u8> = vec![0];
+        let mut read_data2_L: Vec<u8> = vec![0];
+        let mut read_data3_H: Vec<u8> = vec![0];
+        let mut read_data3_L: Vec<u8> = vec![0];
+        let mut read_data4_H: Vec<u8> = vec![0];
+        let mut read_data4_L: Vec<u8> = vec![0];
 
         let mut sensor_val_from_slave0 :[u16; 4] = [0; 4];
         let mut sensor_val_from_slave1 :[u16; 4] = [0; 4];
@@ -225,6 +225,17 @@ fn main() {
             println!("1:{:?}", sensor_val_from_slave1);
             println!("2:{:?}", sensor_val_from_slave2);
 
+            let sensor_val_circle = [
+                sensor_val_from_slave0[0], sensor_val_from_slave1[0], sensor_val_from_slave2[0],
+                sensor_val_from_slave0[1], sensor_val_from_slave1[1], sensor_val_from_slave2[1],
+                sensor_val_from_slave0[2], sensor_val_from_slave1[2], sensor_val_from_slave2[2],
+                sensor_val_from_slave0[3], sensor_val_from_slave1[3], sensor_val_from_slave2[3],
+            ];
+
+            let sensor_val_circle_sort_handle :uint = [];
+
+            
+
             /*
             let sensor_val_r = [
                 (1000.0 / (sensor_val[0] as f32).sqrt()),
@@ -262,18 +273,18 @@ fn main() {
         let (direction_sceta, power_u8) = *(from_controller_params.lock().unwrap());
         let power: f64 = power_u8 as f64 / 255.0 as f64;
 
-        let robot_dir :u16 = 270; //controll
+        let robot_dir: u16 = 270; //controll
 
-        let sensor_dir :u16 = 360 - *(from_sensor_dir.lock().unwrap()); //0~360, reverse
-        let sensor_dir_diff :i16 = sensor_dir as i16 - robot_dir as i16;
-        let mod_robot_dir :u16 = if sensor_dir_diff < 0 { (360 + sensor_dir_diff) as u16 } else { sensor_dir_diff as u16 }; //centelyzed by robot_dir
+        let sensor_dir: u16 = 360 - *(from_sensor_dir.lock().unwrap()); //0~360, reverse
+        let sensor_dir_diff: i16 = sensor_dir as i16 - robot_dir as i16;
+        let mod_robot_dir: u16 = if sensor_dir_diff < 0 { (360 + sensor_dir_diff) as u16 } else { sensor_dir_diff as u16 }; //centelyzed by robot_dir
 
-        let direction_sceta_dig : f64 = (direction_sceta as f64) / (360 as f64) * 2.0 * PI;
-        let direction_sceta_dig_x_y : [f64; 2] = [direction_sceta_dig.cos(), direction_sceta_dig.sin()];
+        let direction_sceta_dig: f64 = (direction_sceta as f64) / (360 as f64) * 2.0 * PI;
+        let direction_sceta_dig_x_y: [f64; 2] = [direction_sceta_dig.cos(), direction_sceta_dig.sin()];
 
-        let mut motor1 :f64 = direction_sceta_dig_x_y[0] * motor_dir_x_y[0][0] + direction_sceta_dig_x_y[1] * motor_dir_x_y[0][1];
-        let mut motor2 :f64 = direction_sceta_dig_x_y[0] * motor_dir_x_y[1][0] + direction_sceta_dig_x_y[1] * motor_dir_x_y[1][1];
-        let mut motor3 :f64 = direction_sceta_dig_x_y[0] * motor_dir_x_y[2][0] + direction_sceta_dig_x_y[1] * motor_dir_x_y[2][1];
+        let mut motor1: f64 = direction_sceta_dig_x_y[0] * motor_dir_x_y[0][0] + direction_sceta_dig_x_y[1] * motor_dir_x_y[0][1];
+        let mut motor2: f64 = direction_sceta_dig_x_y[0] * motor_dir_x_y[1][0] + direction_sceta_dig_x_y[1] * motor_dir_x_y[1][1];
+        let mut motor3: f64 = direction_sceta_dig_x_y[0] * motor_dir_x_y[2][0] + direction_sceta_dig_x_y[1] * motor_dir_x_y[2][1];
 
         let max_num_rev: f64 = 1.0 / ( motor1.max(motor2).max(motor3) );
 
@@ -286,8 +297,8 @@ fn main() {
         motor3 = motor3 * power;
         
         //senosrs dir feedback
-        let dir_diff_angle :f64 = if mod_robot_dir <= 180 { -1.0 * (mod_robot_dir as f64) } else { 360.0 - (mod_robot_dir as f64) }; //-180~180 //mod is centelyzed by robot_dir
-        let mut dir_diff :f64 = dir_diff_angle / 180.0; // -1.0~1.0
+        let dir_diff_angle: f64 = if mod_robot_dir <= 180 { -1.0 * (mod_robot_dir as f64) } else { 360.0 - (mod_robot_dir as f64) }; //-180~180 //mod is centelyzed by robot_dir
+        let mut dir_diff: f64 = dir_diff_angle / 180.0; // -1.0~1.0
 
         dir_diff = dir_diff.clamp(-0.5, 0.5);
 
@@ -304,7 +315,7 @@ fn main() {
         error[0] = error[1];
         error[1] = dir_diff;
         integral += (error[0] + error[1])/2.0 * time_after_command;
-        let delta_dir_motion :f64 = KP*error[1] + KI*integral + (KD*((error[1] - error[0]) / time_after_command));
+        let delta_dir_motion: f64 = KP*error[1] + KI*integral + (KD*((error[1] - error[0]) / time_after_command));
 
         //println!("integral: {}", integral);
         //println!("delta_dir_motion: {}", delta_dir_motion);
@@ -334,7 +345,7 @@ fn main() {
         //save motor
         if motor1.abs() < 0.15 && motor2.abs() < 0.15 && motor3.abs() < 0.15 { motor1 = 0.0; motor2 = 0.0; motor3 = 0.0; }
 
-        let motor :[i8; 3] = [(motor1*100.0) as i8, (motor2*100.0) as i8, (motor3*100.0) as i8]; //should -100 to 100
+        let motor: [i8; 3] = [(motor1*100.0) as i8, (motor2*100.0) as i8, (motor3*100.0) as i8]; //should -100 to 100
         let mut cmd_str =  String::from("1F000"); //unused motor channel 1
         let cmd = generate_cmd(&motor, &mut cmd_str);
 
